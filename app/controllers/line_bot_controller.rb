2037f7f6de5fb008c781ddd7b3972fedf740b7f8
向井優一 <yuichi-m@weseek.co.jp>
2021-07-14 09:52:00 +0900

    complete return sametext bot

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,6 +2,33 @@ class LineBotController < ApplicationController
   protect_from_forgery except:[:callback]
 
   def callback
+    #リクエストのメッセージボディを参照
+    #readで文字列として取得
+    body = request.body.read
+    #署名の検証のためヘッダーを参照する
+    #HTTP_X_LINE_SIGNATUREに署名が格納されている
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    #署名検証
+    unless client.validate_signature(body, signature)
+      #headメソッドはステータスコードを返すために使用
+      return head :bad_request
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      #ユーザーがメッセージ送信したことを示すイベントかどうか確認
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
   end
 
   private 
